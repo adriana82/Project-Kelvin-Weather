bfbb566bf13578c100b85421b1232d03605f8c2c
adriana82 <32023795+adriana82@users.noreply.github.com>
2017-10-03 16:37:46 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const kelvin= 294;
+//we are not gonna change the value of kelvin
+
+const celsius= kelvin - 273;
+// we are not gonna change the value of celsius
+
+let fahrenheit = celsius * (9/5) + 32;
+// comment
+
+fahrenheit= Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`);
